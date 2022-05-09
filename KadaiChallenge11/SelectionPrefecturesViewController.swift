092ce7cbdcc2db35ef571695bdf14f808b903cb5
Sena <senana0116@icloud.com>
2022-05-09 16:26:28 +0900

    課題チャレンジ１１

--- KadaiChallenge11/SelectionPrefecturesViewController.swift
+++ KadaiChallenge11/SelectionPrefecturesViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class SelectionPrefecturesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet private weak var tableView: UITableView!
     private let prefecturesList = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
                                    "山形県", "福島県", "茨城県", "栃木県", "群馬県",
                                    "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県",
@@ -49,7 +49,7 @@ class SelectionPrefecturesViewController: UIViewController, UITableViewDelegate,
         performSegue(withIdentifier: "exitFromSelectionPrefecturesSegue", sender: nil)
     }
 
-    @IBAction func cancelButton(_ sender: Any) {
+    @IBAction private func cancelButton(_ sender: Any) {
         dismiss(animated: true)
     }
 }
